72bf7613efca242d950d55c36c84d5b7b07b7e79
Sujan-321 <132794544+Sujan-321@users.noreply.github.com>
2025-11-17 18:35:04 +0545

    remove impurities from database.py, models.py and session.py file

--- app/db/session.py
+++ app/db/session.py
@@ -11,22 +11,3 @@ def init_db():
     Base.metadata.create_all(bind=engine)
 
 
-
-
-
-
-# # app/db/session.py
-# import os
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from app.db.models import Base
-
-# DB_URL = os.getenv("DATABASE_URL", "sqlite:///./app_data.db")
-# # For SQLite, allow check_same_thread for FastAPI concurrency
-# engine = create_engine(
-#     DB_URL, connect_args={"check_same_thread": False} if DB_URL.startswith("sqlite") else {}
-# )
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# def init_db():
-#     Base.metadata.create_all(bind=engine)
